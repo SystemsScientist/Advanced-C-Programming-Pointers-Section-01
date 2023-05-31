438c18c503dcc353b15dc13e1193e42e498f9573
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-31 16:51:33 -0500

    Adding 07-Indirection files and directories

---
+++ 07-Indirection/main.c
@@ -0,0 +1,24 @@
+/*
+    Author: Matt Johnson
+    Subject: indirection (or dereferencing)
+    Purpose:
+    Date: 05/31/2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    int num;
+    int *numPtr;
+    int num2;
+
+    num = 100;
+    numPtr = &num;
+    num2 = *numPtr;
+
+    printf("num = %d, numPtr = %p, address of num = %p, num2 = %d\n", num, numPtr, &num, num2);
+
+    return 0;
+}
